3698899bd5af7d3184a4a7774890fd9a969988cd
LuisAbdielRivera <147383444+LuisAbdielRivera@users.noreply.github.com>
2024-05-22 20:28:55 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace clienteServidor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewClientes = new System.Windows.Forms.DataGridView();
+            this.InsertarCliente = new System.Windows.Forms.Button();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.tbNombre = new System.Windows.Forms.TextBox();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewClientes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewClientes
+            // 
+            this.dataGridViewClientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewClientes.Location = new System.Drawing.Point(12, 12);
+            this.dataGridViewClientes.Name = "dataGridViewClientes";
+            this.dataGridViewClientes.ReadOnly = true;
+            this.dataGridViewClientes.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridViewClientes.Size = new System.Drawing.Size(394, 209);
+            this.dataGridViewClientes.TabIndex = 0;
+            this.dataGridViewClientes.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewClientes_CellClick);
+            this.dataGridViewClientes.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewClientes_CellContentClick);
+            // 
+            // InsertarCliente
+            // 
+            this.InsertarCliente.Location = new System.Drawing.Point(460, 123);
+            this.InsertarCliente.Name = "InsertarCliente";
+            this.InsertarCliente.Size = new System.Drawing.Size(75, 23);
+            this.InsertarCliente.TabIndex = 1;
+            this.InsertarCliente.Text = "Nuevo";
+            this.InsertarCliente.UseVisualStyleBackColor = true;
+            this.InsertarCliente.Click += new System.EventHandler(this.InsertarCliente_Click);
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(460, 51);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(44, 13);
+            this.lblNombre.TabIndex = 2;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // tbNombre
+            // 
+            this.tbNombre.Location = new System.Drawing.Point(559, 44);
+            this.tbNombre.Name = "tbNombre";
+            this.tbNombre.Size = new System.Drawing.Size(100, 20);
+            this.tbNombre.TabIndex = 3;
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Location = new System.Drawing.Point(559, 122);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(75, 23);
+            this.btnActualizar.TabIndex = 4;
+            this.btnActualizar.Text = "Modificar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.tbNombre);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.InsertarCliente);
+            this.Controls.Add(this.dataGridViewClientes);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewClientes)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewClientes;
+        private System.Windows.Forms.Button InsertarCliente;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.TextBox tbNombre;
+        private System.Windows.Forms.Button btnActualizar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using clienteServidor.ServiceReference1;
+
+namespace clienteServidor
+{
+    public partial class Form1 : Form
+    {
+        private ClienteServiceClient clienteServiceClient;
+
+        public Form1()
+        {
+            InitializeComponent();
+            clienteServiceClient = new ClienteServiceClient();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CargarClientes();
+        }
+
+        private void CargarClientes()
+        {
+            var clientes = clienteServiceClient.ObtenerClientes();
+            dataGridViewClientes.DataSource = clientes;
+        }
+
+        private void dataGridViewClientes_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            CargarClientes();
+        }
+
+        private void dataGridViewClientes_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            var cellValue = dataGridViewClientes.CurrentRow.Cells[2].Value;
+            tbNombre.Text = cellValue.ToString();
+
+        }
+
+        private void InsertarCliente_Click(object sender, EventArgs e)
+        {
+            var nuevoCliente = new Cliente
+            {
+                Nombre = tbNombre.Text,
+                FechaNacimiento = DateTime.Now
+            };
+
+            clienteServiceClient.InsertarCliente(nuevoCliente);
+            CargarClientes();
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            Cliente clienteActualizar = (Cliente)dataGridViewClientes.SelectedRows[0].DataBoundItem;
+
+            clienteActualizar.Nombre = tbNombre.Text;
+
+            clienteServiceClient.ModificarCliente(clienteActualizar);
+            CargarClientes();
+        }
+
+
+    }
+}
